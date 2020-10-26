1a7387a41cf75d51f62bc713bd94f03c14dab78c
ladisho <20084356@mail.wit.ie>
2020-10-26 17:11:46 +0000

    Add to favorites logic completed; Dummy favorites page.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
+import StubAPI from "../api/stubAPI";
 
 
 
@@ -38,11 +39,24 @@ const MovieListPage = () => {
         else setGenreFilter(value);
     };
     // NEW BLOCK END
+    const addToFavorites = movieId => {
+        // Find index position of selected movie in the list
+        const index = movies.map(m => m.id).indexOf(movieId)
+
+        StubAPI.add(movies[index])
+        const updatedList = [...movies]  // Make a copy of the movie list
+        updatedList.splice(index, 1)     // Remove selected movie from home page list
+        setMovies(updatedList)
+    }
+
     return (
         <>
             <Header numMovies={displayedMovies.length} />          {/* CHANGED */}
             <FilterControls onUserInput={handleFilterChange} />    {/* CHANGED */}
-            <MovieList movies={displayedMovies} />                  {/* CHANGED */}
+            <MovieList
+                movies={displayedMovies}
+                buttonHandler={addToFavorites}
+            />     {/* CHANGED */}
         </>
     );
 };
