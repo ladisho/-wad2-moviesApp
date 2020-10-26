861cb30acc406ef2dd7a8a146c720a5dd770e0c7
ladisho <20084356@mail.wit.ie>
2020-10-26 13:42:31 +0000

    Movie Details Page - included production countries

--- src/index.js
+++ src/index.js
@@ -13,64 +13,88 @@ const sample = {
         poster_path: "/iTQHKziZy9pAAY4hHEDCGPaOvFC.jpg",
         backdrop_path: "/d8duYyyC9J5T825Hg7grmaabfxQ.jpg"
     },
-    budget: 200000000,
+    budget: 55000000,
     genres: [
         {
             id: 14,
-            name: "Fantasy"
+            name: "Crime"
         },
         {
             id: 12,
-            name: "Adventure"
+            name: "Thriller"
         },
         {
             id: 878,
-            name: "Science Fiction"
-        },
-        {
-            id: 28,
-            name: "Action"
+            name: "Drama"
         }
     ],
     homepage:
-        "https://www.starwars.com/films/star-wars-episode-viii-the-last-jedi",
+        "https://http://www.jokermovie.net/",
     id: 181808,
     imdb_id: "tt2527336",
     original_language: "en",
-    original_title: "Star Wars: The Last Jedi",
+    original_title: "Joker",
     overview:
-        "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+        "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
     popularity: 44.208,
-    poster_path: "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+    poster_path: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
     production_companies: [
         {
             id: 1,
             logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
-            name: "Lucasfilm",
+            name: "DC Entertainment",
             origin_country: "US"
         },
         {
             id: 11092,
             logo_path: null,
-            name: "Ram Bergman Productions",
+            name: "Warner Bros Pictures",
             origin_country: "US"
         },
         {
             id: 2,
             logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
-            name: "Walt Disney Pictures",
+            name: "DC Comics",
+            origin_country: "US"
+        },
+        {
+            id: 11093,
+            logo_path: null,
+            name: "Joint Effort",
+            origin_country: "US"
+        },
+        {
+            id: 3,
+            logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
+            name: "Village Roadshow Pictures",
+            origin_country: "US"
+        },
+        {
+            id: 11094,
+            logo_path: null,
+            name: "Bron Studios",
+            origin_country: "US"
+        },
+        {
+            id: 4,
+            logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
+            name: "Creative Wealth Media Finance",
             origin_country: "US"
         }
     ],
     production_countries: [
         {
+            iso_3166_1: "CA",
+            name: "Canada"
+        },
+        {
             iso_3166_1: "US",
             name: "United States of America"
         }
     ],
-    release_date: "2017-12-13",
-    revenue: 1332459537,
-    runtime: 152,
+    release_date: "2019-10-02",
+    revenue: 1074251311,
+    runtime: 122,
     spoken_languages: [
         {
             iso_639_1: "en",
@@ -78,8 +102,8 @@ const sample = {
         }
     ],
     status: "Released",
-    tagline: "Darkness rises... and light to meet it",
-    title: "Star Wars: The Last Jedi",
+    tagline: "Put on a happy face.",
+    title: "Joker",
     video: false,
     vote_average: 7,
     vote_count: 9692
