0ac2af419fb7d24c4878585c7ec6f166299b0ccd
ladisho <20084356@mail.wit.ie>
2020-11-04 13:17:51 +0000

    Refactor: Movie list and genre list now stored in contexts

--- src/index.js
+++ src/index.js
@@ -8,21 +8,27 @@ import FavoriteMoviesPage from './pages/favoritesMoviesPage'       // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
+import MoviesContextProvider from "./contexts/moviesContext";
+import GenresContextProvider from "./contexts/genresContext";
 
 const App = () => {
     return (
         <BrowserRouter>
             <div className="jumbotron">
-                <SiteHeader />      {/* New Header  */}
+                <SiteHeader />
                 <div className="container-fluid">
-                    <Switch>
-                        <Route path="/movies/upcoming" component={UpcomingMoviesPage} />
-                        <Route path="/reviews/:id" component={MovieReviewPage} />
-                        <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-                        <Route path="/movies/:id" component={MoviePage} />
-                        <Route path="/" component={HomePage} />
-                        <Redirect from="*" to="/" />
-                    </Switch>
+                    <MoviesContextProvider>
+                        <GenresContextProvider>    {/* NEW */}
+                            <Switch>
+                                <Route path="/movies/upcoming" component={UpcomingMoviesPage} />
+                                <Route path="/reviews/:id" component={MovieReviewPage} />
+                                <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+                                <Route path="/movies/:id" component={MoviePage} />
+                                <Route path="/" component={HomePage} />
+                                <Redirect from="*" to="/" />
+                            </Switch>
+                        </GenresContextProvider>    {/* NEW */}
+                    </MoviesContextProvider>
                 </div>
             </div>
         </BrowserRouter>
